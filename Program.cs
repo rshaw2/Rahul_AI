34fb6dec9b0b5ce3ec49e1c69eb4c9d113d0a6da
Rahul Kumar Shaw <69197147+rshaw2@users.noreply.github.com>
2024-02-23 12:03:06 +0530

    Upload files from codezen

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using RahulAI.Data;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Low Code", Version = "v1" });
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+builder.Services.AddControllers();
+builder.Services.AddTransient<RahulAIContext>();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.UseAuthentication();
+app.UseAuthorization();
+app.Run();
